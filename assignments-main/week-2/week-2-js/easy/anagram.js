305d06d33f281ea05ed4c5bb9ca024b10af90061
GUJJULA_BHAVANA <bg0586@srmist.edu.in>
2025-01-26 23:56:57 +0530

    doing week-2 assignments

--- assignments-main/week-2/week-2-js/easy/anagram.js
+++ assignments-main/week-2/week-2-js/easy/anagram.js
@@ -4,8 +4,22 @@
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 
-function isAnagram(str1, str2) {
+/*
+Idea:First we need to split the string into array of characters and then sort the array and join 
+the functions we will be using:split(""), sort(), join("")
+*/
 
+function isAnagram(str1, str2) {
+  const lowerCaseString1 = str1.toLowerCase().split("").sort().join("");
+  
+  const lowerCaseString2 = str2.toLowerCase().split("").sort().join("");
+
+  if(sortedString1 == sortedString2){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 module.exports = isAnagram;
