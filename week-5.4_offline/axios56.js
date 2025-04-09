a7742f34e11a6b15c0eff5a107328a52d088bd76
GUJJULA_BHAVANA <bg0586@srmist.edu.in>
2025-04-09 18:03:30 +0530

    axios is completed

--- week-5.4_offline/axios56.js
+++ week-5.4_offline/axios56.js
@@ -1,7 +1,18 @@
 const axios = require("axios");
 
+//POST
+//change request method
+//send body
+//send headers
 async function main() {
-    const response = await axios.post("https://sum-server.100xdevs.com/todos");
+    const response = await axios.post("https://wwww.toptal.com/developers/postbin/1706261117587-5522551864851", {
+        username: "harkirat",
+        password: "1234567"
+    }, {
+        headers: {
+            "Authorization" : "Bearer 123"
+        }
+    });
     console.log(response.data);
 }
 
--- week-5.4_offline/fetch56.js
+++ week-5.4_offline/fetch56.js
@@ -4,6 +4,13 @@ async function main() {
     const response = await fetch("https://wwww.toptal.com/developers/postbin/1706261117587-5522551864851",
         {
             method: "POST",
+            body: {
+                username: "harkirat",
+                password: "124567"
+            },
+            headers: {
+                "Authorization" : "Bearer 123"
+            }
         },
     );
     const textualData = await response.text();
