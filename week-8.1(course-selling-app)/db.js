1a2b2383e539b4a376de2da8e1591c05135fc89d
GUJJULA_BHAVANA <bg0586@srmist.edu.in>
2025-08-10 15:39:08 +0530

    database connection is done

---
+++ week-8.1(course-selling-app)/db.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+console.log("connected to");
+mongoose.connect("mongodb+srv://gujjula_bhavana:Bhavan%40143@cluster0.h0zkntc.mongodb.net/course-selling-app")
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.ObjectId;
+
+ const userSchema = new Schema({
+    email: { type: String, unique: true },
+    password: String,
+    firstName: String,
+    lastName: String,
+    id: ObjectId
+ });
+
+ const adminSchema = new Schema({
+    email: { type: String, unique: true},
+    password: String,
+    firstName: String,
+    lastName: String,
+ });
+
+const courseSchema = new Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    creatorId: ObjectId,
+});
+
+const purchaseSchema = new Schema ({
+    userId: ObjectId,
+    courseId: ObjectId
+});
+
+const UserModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const purchaseModel = mongoose.model("purchase", purchaseSchema);
+
+module.exports = {
+    UserModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
--- week-8.1(course-selling-app)/routes/admin1.js
+++ week-8.1(course-selling-app)/routes/admin1.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const adminRouter = Router();
+const adminSchema = require("../db");
 
 adminRouter.post("/signup", function(req, res){
     res.json({
